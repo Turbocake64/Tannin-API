d2bbacdf7ca919a620fd601e7eae29b2e5821e8a
QuantumPhilosopher <16587451+QuantumPhilosophy@users.noreply.github.com>
2019-07-17 15:15:57 -0700

    reorg for routes and api index.js files

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
-const path = require('path')
 const router = require('express').Router()
 const apiRoutes = require('./api')
 
-// modulizing the routes by requiring the api folder; which tells it to read inside the index.js
-// accessing the folder api (books.js, google.js)
+// modularizing the routes by requiring the api folder; which tells it to read inside the index.js
+// accessing the api route files for login/signup and for adding/getting/deleting wines, restaurants and employees
 router.use('/api', apiRoutes)
 
-// accessing the client folder (index.html)
-// router.use((req, res) =>
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"))
-// );
+// root route
+router.use(function(req, res) {
+  res.json('Welcome to Tannin')
+})
 
 module.exports = router
