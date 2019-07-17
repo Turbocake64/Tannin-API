52e32a8c2e3401a78e0604be5bf9aac6ccbd41c0
QuantumPhilosopher <16587451+QuantumPhilosophy@users.noreply.github.com>
2019-07-17 14:53:04 -0700

    reorg server.js

--- server.js
+++ server.js
@@ -7,15 +7,24 @@ const mongoose = require('mongoose')
 // const db = require('./models/Employees')
 const passport = require('./passport')
 const routes = require('./routes')
-
 const PORT = process.env.PORT || 3001
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/tannin-dev'
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(routes)
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/tannin-dev'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
 // console.log(db)
 // app.use(
@@ -26,24 +35,10 @@ app.use(cors())
 //     saveUninitialized: false
 //   }))
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-
-// if (process.env.NODE_ENV === 'production') {
-//   const path = require('path')
-//   app.use('/static', express.static(path.join(__dirname, '../build/static')))
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../build/'))
-//   })
-// }
-
-app.use(routes)
-
 app.listen(PORT, function () {
-  console.log('Listening on port %s.', PORT)
+  console.log(
+    '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+    PORT,
+    PORT
+  )
 })
